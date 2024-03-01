890e122d66b082e15f62053139c4eabc3c61cb85
llipengda <lipengda2@outlook.com>
2024-03-02 01:30:33 +0800

    Refactor TabBar component to handle click event and scroll to top

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Image, Text, View } from '@tarojs/components'
+import Taro from '@tarojs/taro'
 import { TAB_BAR_LIST } from '@/common/tabBarList'
 import useTabBarStore from '@/stores/tabBarStore'
 
 const TabBar = React.memo(() => {
   const { active, setActive } = useTabBarStore()
+
+  const handleClick = (index: number) => () => {
+    Taro.pageScrollTo({ scrollTop: 0 })
+    setActive(index)
+  }
 
   return (
     <View className='fixed bottom-0 pb-[env(safe-area-inset-bottom)] w-screen bg-white flex flex-row items-center justify-around z-10 h-[calc(142px+env(safe-area-inset-bottom))]'>
@@ -12,7 +18,7 @@ const TabBar = React.memo(() => {
         <View
           key={index}
           className='flex flex-col items-center justify-center mb-[10px]'
-          onClick={() => setActive(index)}
+          onClick={handleClick(index)}
         >
           <View
             className={`w-[64px] h-[64px] mb-2 ${index === 0 ? 'ml-[12%]' : ''}`}
