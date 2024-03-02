091a7431cb57f6bec3e87f6eda71583880176588
llipengda <lipengda2@outlook.com>
2024-03-02 11:08:52 +0800

    Refactor LoginStore types and remove unnecessary code

--- src/stores/loginStore.ts
+++ src/stores/loginStore.ts
@@ -4,18 +4,17 @@ import TaroStorage from '@/utils/TaroStorage'
 import logger from '@/utils/logMiddleware'
 
 type LoginStore = {
-  token?: string
-  phone?: string
+  token: string | undefined
+  phone: string | undefined
   setPhone: (phone: string) => void
   setToken: (token: string) => void
 }
 
 const useLoginStore = create<LoginStore>()(
   persist(
-    // TODO: Remove this in production
     logger(set => ({
-      token: undefined as string | undefined,
-      phone: undefined as string | undefined,
+      token: undefined,
+      phone: undefined,
       setPhone: phone => set({ phone }),
       setToken: token => set({ token })
     })),
