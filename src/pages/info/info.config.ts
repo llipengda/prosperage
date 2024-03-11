f931a1f662bcd487f94c5c4b0fc34f80a66ff380
llipengda <lipengda2@outlook.com>
2024-03-11 16:43:39 +0800

    Add info page configuration and component

---
+++ src/pages/info/info.config.ts
@@ -0,0 +1,4 @@
+export default definePageConfig({
+  navigationBarTitleText: '个人资料',
+  navigationStyle: 'custom'
+})
---
+++ src/pages/info/info.tsx
@@ -0,0 +1,7 @@
+import { View } from '@tarojs/components'
+
+const Info = () => {
+  return <View></View>
+}
+
+export default Info
